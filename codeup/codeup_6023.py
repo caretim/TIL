1ec4007930cebbeb3e207bbf25b079de90bbf63a
caretim <karesin72041@gmail.com>
2022-07-11 17:33:04 +0900

    python upate/220711

---
+++ codeup/codeup_6023.py
@@ -0,0 +1,8 @@
+# a = input().split(':')
+
+
+# print(a[3:5])
+
+a,b,c= input().split(':')
+
+print(b)
---
+++ codeup/codeup_6026.py
@@ -0,0 +1,14 @@
+# a,b = input().split()
+
+# a = float(a)
+# b = float(b)
+
+# print(a+b)
+
+
+a = float(input())
+b = float(input())
+
+print(a+b)
+
+
