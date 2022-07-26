8e9fb53d74f394ac96e7dd7cae23b51b10274979
caretim <karesin72041@gmail.com>
2022-07-26 23:17:08 +0900

    20220726

--- SWEA/D2/1859.py
+++ SWEA/D2/1859.py
@@ -1,22 +1,39 @@
 #인덱스 값을 한번 진행할때마다 더해주는 값을 n*i (증가값 곱하기 더하기 횟수)
 
-
-n =int(input())
-
-price = list(map(int,input().split()))
-
-cnt=1 # 연달아 물건을 사야할때 카운트 다음 물건의 값이 크다면     
-        #값을 cnt_price에 보내준뒤 0으로 초기화 # n
+import sys
 
 
-cnt_price= 0 # 
+sys.stdin = open("input.txt", "r")
 
-for i in range(n):  #마지막 인덱스가 범위를 벗어나게됨.. 그럼 음수값으로 지정할까?
-    if price[i] <= price[i+1] :
-        p = price[i+1]-(price[i])
-        cnt_price+= p*cnt
-        cnt +=1 
-    else :
-        cnt =0
 
-print(cnt_price)
+T = int(input())
+# 여러개의 테스트 케이스가 주어지므로, 각각을 처리합니다.
+for test_case in range(1, T + 1):
+
+    n =int(input())
+
+    price = list(map(int,input().split()))
+
+    cnt=1 # 연달아 물건을 사야할때 카운트 다음 물건의 값이 크다면     
+            #값을 cnt_price에 보내준뒤 0으로 초기화 # n
+
+    sq= 0
+    cnt_price= 0 # 
+    low_price=[]
+
+    for i in range(n):  #마지막 인덱스가 범위를 벗어나게됨.. 그럼 음수값으로 지정할까?
+        if sq == n-1: # 작고 작을때 구매를 안하게됨 > > 
+            break
+        elif price[i] <= price[i+1] :
+            p = price[i+1]-(price[i])
+            cnt_price+= p*cnt
+            cnt +=1 
+            
+        else :
+            cnt =1
+            
+        sq +=1
+
+    print(f'#{test_case} {cnt_price}')
+
+    #처음부터 새로 짜야할듯.
--- SWEA/D2/test.py
+++ SWEA/D2/test.py
@@ -1,9 +1,5 @@
-n = int(input())
+anw =  'oxoxoxox'
 
-for i in range(1, n + 1):
-    i = str(i)
-    game = i.count('3') + i.count('6') + i.count('9')
-    if game == 0:
-        print(i, end=' ')
-    else:
-        print('-'*game, end=' ')
+
+for i in anw:
+    print (i)
