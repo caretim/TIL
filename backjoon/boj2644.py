b7ebc25c8c64bd1751a25a5692e9a44ab7444def
caretim <karesin72041@gmail.com>
2022-08-19 00:20:07 +0900

    20220819

--- backjoon/boj2644.py
+++ backjoon/boj2644.py
@@ -1,6 +1,7 @@
 
 n = int(input())
 
+
 ch1,ch2= map(int,input().split())
 
 m=int(input())
@@ -13,27 +14,35 @@ for __ in range(m):
     u,v= map(int,input().split())
     node[u].append(v)
     node[v].append(u)
-
 def fam (x,y):
-    cnt=0
+    #cnt=0
+    #recnt=0
     stack=[]
     visit[x]=True
-    stack.append(x)
+    stack.append((x,1))
+    cntls= []
     while stack:
         s =stack.pop()
-        cnt+=1
-        sq=0
-        for j in node[s]:
+        #cnt+=1
+        #sq=0
+        for j in node[s[0]]:
             if j==y :
-                return cnt
-            if visit[j] == False:
+                cntls.append(s[1])
+            elif visit[j] == False:
                 visit[j]= True
-                stack.append(j)
-                sq=1
-        if sq ==0:
-            cnt-=1
-    return -1
+                stack.append((j,s[1]+1))
+                #sq=1
+        # if sq ==0:
+        #     cnt-=recnt
+        #     recnt=0
+    #print(cntls)
+    if len(cntls)>= 1:
+        return min(cntls)
+    elif len(cntls)==0:
+        return '-1'
+    
+
                 
 
-
+# print(node)
 print(fam(ch1,ch2))
