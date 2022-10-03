5c33c7dc78903a372a27675dc0261cc6a3fd4876
caretim <karesin72041@gmail.com>
2022-10-03 23:21:19 +0900

    221003

--- WID_T/0928/boj10815.py
+++ WID_T/0928/boj10815.py
@@ -8,24 +8,42 @@ m = int(input())
 
 nums = list(map(int, input().split()))
 
-
-nums.sort()
-
+sang.sort()
 
 result = []
-for i in range(n):
+
+for i in range(m):
     start = 0
-    end = len(nums) - 1
-    mid = start + end // 2
+    end = len(sang) - 1
     check = 0
     while start <= end:
-        if nums[mid] == sang[i]:
+        mid = (start + end) // 2
+        if sang[mid] == nums[i]:
             check = 1
             break
-        elif nums[mid] > sang[i]:
+        elif sang[mid] > nums[i]:
             end = mid - 1
-        elif nums[mid] < sang[i]:
+        elif sang[mid] < nums[i]:
             start = mid + 1
-    nums.append("0")
+    print(check, end=" ")
 
-print(result)
+
+# n = int(input())
+# N = list(map(int, input().split()))
+# N = sorted(N)
+# m = int(input())
+# M = list(map(int, input().split()))
+
+# for i in M:
+#     low, high = 0, n - 1
+#     check = 0
+#     while low <= high:
+#         mid = (low + high) // 2
+#         if N[mid] > i:
+#             high = mid - 1
+#         elif N[mid] < i:
+#             low = mid + 1
+#         else:
+#             check = 1
+#             break
+#     print(check, end=" ")
