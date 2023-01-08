3807aa8248462719575d48ce76b3fe4d293e402c
caretim <karesin72041@gmail.com>
2023-01-08 23:53:25 +0900

    230108

---
+++ WID_T/230106/2xn타일링.py
@@ -0,0 +1,20 @@
+
+# 1일때 1 , 2일때 2 3일때 3 4일때 5 피보나치수열문제랑 앞의 인덱수 두개를 더한값
+
+
+
+n= int(input())
+
+dp = [0]*(n+1)
+
+
+dp[1]=1
+dp[2]=2
+
+if n<=2:
+    print(dp[n])
+
+for i in range(3,n+1):
+    dp[i]=dp[i-1]+dp[i-2]
+    if i == n:
+        print(dp[n]%10007)
---
+++ WID_T/230106/개미군단.py
@@ -0,0 +1,11 @@
+hp=24
+def solution(hp):
+    answer = 0
+    answer+=hp//5
+    hp = hp%5
+    answer+=hp//3
+    hp = hp%3
+    answer+=hp
+    return answer
+
+print(solution(hp))
