bad65034c27cdf1ea386ea52e38e37f1bdb29890
caretim <karesin72041@gmail.com>
2023-01-19 00:30:01 +0900

    230119

--- WID_T/230117/토마토.py
+++ WID_T/230117/토마토.py
@@ -1,5 +1,46 @@
-n, m = map(int, input().split())
+# n, m = map(int, input().split())
 
+
+# matrix = [list(map(int, input().split())) for __ in range(m)]
+
+# dx = [0, 0, 1, -1]
+# dy = [-1, 1, 0, 0]
+
+# check = [[0] * n for __ in range(m)]
+
+# result = 0
+
+
+# while True:
+#     ripe = []
+#     for y in range(m):
+#         for x in range(n):
+#             if (matrix[y][x], check[y][x]) == (1, 0):
+#                 check[y][x] = 1
+#                 for i in range(4):
+#                     ny = y + dy[i]
+#                     nx = x + dx[i]
+#                     if 0 <= ny < m and 0 <= nx < n:
+#                         if matrix[ny][nx] == 0:
+#                             ripe.append((ny, nx))
+#     ripe = set(ripe)
+#     if len(ripe) > 0:
+#         for y, x in ripe:
+#             matrix[y][x] = 1
+#         result += 1
+#     else:
+#         break
+
+# result_sum = 0
+# for i in matrix:
+#     if i.count(0):
+#         result = -1
+
+# print(result)
+
+from collections import deque
+
+n, m = map(int, input().split())
 
 matrix = [list(map(int, input().split())) for __ in range(m)]
 
@@ -10,28 +51,27 @@ check = [[0] * n for __ in range(m)]
 
 result = 0
 
+start_list = []
+for y in range(m):
+    for x in range(n):
+        if matrix[y][x] == 1:
+            start_list.append((y, x, 0))
+            check[y][x] = 1
 
-while True:
-    ripe = []
-    result += 1
-    for y in range(m):
-        for x in range(n):
-            if (matrix[y][x], check[y][x]) == (1, 0):
-                print("토마토발견")
-                check[y][x] = 1
-                for i in range(4):
-                    ny = y + dy[i]
-                    nx = x + dx[i]
-                    if 0 <= ny < m and 0 <= nx < n:
-                        if matrix[ny][nx] == 0:
-                            ripe.append((ny, nx))
-                            print("토마토담기")
-    ripe = set(ripe)
-    print(ripe)
-    if len(ripe) > 0:
-        for y, x in ripe:
-            matrix[y][x] = 1
-    else:
-        break
+q = deque(start_list)
+while q:
+    y, x, c = q.popleft()
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if 0 <= ny < m and 0 <= nx < n:
+            if (matrix[ny][nx], check[ny][nx]) == (0, 0):
+                matrix[ny][nx] = 1
+                check[ny][nx] = 1
+                q.append((ny, nx, c + 1))
 
-print(result)
+for i in matrix:
+    if i.count(0):
+        c = -1
+
+print(c)
