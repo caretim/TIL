ee6db52950ff706f07882fa0f408d6b5353d0c90
caretim <karesin72041@gmail.com>
2023-01-25 14:54:20 +0900

    230125

--- WID_T/221228/뱀과사다리게임.py
+++ WID_T/221228/뱀과사다리게임.py
@@ -1,46 +1,100 @@
+# n, m = map(int, input().split())
+
+
+# board = [[0] for __ in range(101)]
+
+# good = []
+
+# bad = []
+
+# count = 0
+
+
+# for __ in range(n):
+#     x, y = map(int, input().split())
+#     board[x] = y
+#     if y - x > 6:
+#         good.append(x)
+
+# for __ in range(m):
+#     x, y = map(int, input().split())
+#     board[x] = y
+#         bad.append(x)
+
+
+# def throw(a):
+#     global count
+#     count += 1
+#     for i in range(6, 0, -1):
+#         if a + i == 100:
+#             return
+#         elif a + i in good:
+#             count += 1
+#             throw(board[a + i])
+#             return
+#         elif a + i in bad:
+#             pass
+#         else:
+#             count += 1
+#             throw(a + i)
+#             return
+
+
+# throw(1)
+
+# print(count)
+from collections  import deque
+
+dice = [1,2,3,4,5,6]
+
 n, m = map(int, input().split())
 
 
-board = [[0] for __ in range(101)]
+board = [0]*101
+check = [0]*101
 
-print(board)
 good = []
 
 bad = []
-
-count = 0
 
 
 for __ in range(n):
     x, y = map(int, input().split())
     board[x] = y
-    if y - x > 6:
-        good.append(x)
+    check[x] = -2
+    if y-x > 6:
+        good.append((x,y))
 
 for __ in range(m):
     x, y = map(int, input().split())
     board[x] = y
-    bad.append(x)
+    check[x] = -2
+    bad.append((x,y))
 
 
-def throw(a):
-    global count
-    count += 1
-    for i in range(6, 0, -1):
-        if a + i == 100:
-            return
-        elif a + i in good:
-            count += 1
-            throw(board[a + i])
-            return
-        elif a + i in bad:
-            pass
-        else:
-            count += 1
-            throw(a + i)
-            return
+def bfs(x):
+    q = deque()
+    check[x] = 'start'
+    q.append((x,0))
+    while q:
+       x,y= q.popleft()
+       for i in range(6):
+        nx =x+dice[i]
+        if nx < 101:
+            if check[nx]==0:
+                check[nx]=y+1
+                q.append((nx,y+1))
+            if check[nx]==-2:
+                check[nx]=y+1
+                q.append((board[nx],y+1))
 
 
-throw(1)
 
-print(count)
+
+        
+bfs(1)
+print(check[100])
+
+
+
+# 뱀을 피해서 사다리를 타고 올라가야함 사다리가 6칸 이상의 이동을 보장 할 떄 사다리 사용 외에는 주사위와 같다
--- WID_T/230104/숨바꼭질2.py
+++ WID_T/230104/숨바꼭질2.py
@@ -1,28 +1,49 @@
 from collections import deque
 
-n, m = map(int(input().split()))
+n, m = map(int,input().split())
 
-check =[0]*(m+1)
+r = n+m*4+1
 
-dp = [0]*(m+1)
+check =[[-1,0]for __ in range(r)]
 
 
 
 
 #점화식 = x-1, x+1 x*2
+# 도착지점에 도달 한 순간부터 들어오는 m값을 체크 m값과 동일한 것만 넣어주고 그 이상의 시행은 멈춘다.
+result = []
 
-
-def find(n,m):
-    q= deque([n,0])
+def find(n):
+    q= deque()
+    q.append((n,0))
+    check[n][0]=1
+    check[n][1]=0
     while q:
         v,j = q.popleft()
-        if v==m:
-            return(v)
-        for k in (v-1,v+1,v*2):
-            k.append()
+        for k in (v-1,v+1,v*2): 
+            if 0<=k <r:
+                if check[k][0] == -1:
+                    check[k][0]=j+1
+                    check[k][1]=1
+                    q.append((k,j+1))
+                elif check[k][0]==j+1:
+                    check[k][1]+=1
+                    q.append((k,j+1))
+
+                # else: # 동생을 찾고나서의 로직 
+                #     if  j+1 <= meet :
+                #         if (k,j+1) == (m,meet):
+                #             result.append(k)
+                #         elif check[k]==0:
+                #             q.append((k,j+1))
+
+# collect = find(n,m)
+
+# print()
+find(n)
+print(*check[m])
+# print(find(n,m),len(result))
 
 
 
-
-find(n,m)
 
