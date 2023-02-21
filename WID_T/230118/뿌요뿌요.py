40a7dd1420114e0b7953d72876b91df442eae5c2
caretim <karesin72041@gmail.com>
2023-02-22 00:18:09 +0900

    230222

--- WID_T/230118/뿌요뿌요.py
+++ WID_T/230118/뿌요뿌요.py
@@ -1,3 +1,4 @@
+from collections import deque
 
 dy = [0,0,1,-1]
 dx = [1,-1,0,0]
@@ -14,41 +15,57 @@ time =0
 # 3. 부서지고 중력적용은 어떻게 시켜줄까?
 # 3-1. 부순후에 밑의 좌표가 . 일떄 위의 값들을 당겨오는 로직
 # 3-2. 부술 때 
-
+check = []
 def playing():
-    boom=0
+    global matrix
+    global time
+    global check
     check = [[0]*6 for __ in range(12)]
+    boom=0
     for y in range(12):
         for x in range(6):
-            if check[y][x] ==0 and matrix[y][x] in ["Y","G","R","P","B"]:
-                if find(y,x,matrix[y][x]) == True:
+            if check[y][x] ==0 and matrix[y][x]!='.':
+                if find(y,x,matrix[y][x])==1:
                     boom=1
     if boom ==1:
+        time+=1
+        cg=deque()
+        for x in range(6):
+            for y in range(11,-1,-1):
+                if matrix[y][x]!= '.':
+                    cg.append(matrix[y][x])
+            for y in range(11,-1,-1):
+                if cg:
+                    matrix[y][x]=cg.popleft()
+                else:
+                    matrix[y][x]='.'
         playing()
     else:
-        return(time)
-
+        print(time)
 
 
 def find(y,x,p):
     global check
+    global matrix
     pang = 0
-    stack =list((y,x))
-    pang_lsit =[]
-    while stack :
-        stack.pop((y,x))
+    stack =list()
+    stack.append((y,x))
+    pang_list =[]
+    while stack:
+        y,x=stack.pop()    
         for i in range(4):
-            ny,nx=y+ny[i],x+nx[i]
+            ny,nx=y+dy[i],x+dx[i]
             if 0<=ny<12 and 0<=nx<6 :
                 if check[ny][nx]==0 and matrix[ny][nx]==p:
                     stack.append((ny,nx))
-                    pang_lsit.append((ny,nx))
+                    pang_list.append((ny,nx))
+                    check[ny][nx]=1
                     pang+=1
-    if pang >= 4:
-        #뿌요 지우고 중력적용 y축으로 값을 어떤식으로 당겨주지? pop으로 뽑는다면 yx반전시키는방법도 있긴한데 
-        # 
+    if pang >= 4: 
+        #뿌요 지우고 중력적용 y축으로 값을 어떤식으로 당겨주지? pop으로 뽑는다면 yx반전시키는방법도 있긴한데
+        for y,x in pang_list:
+            matrix[y][x]='.'
         return True # 성공했을때 트루만 반환 
 
-
-
-
+#시작! 
+playing()
