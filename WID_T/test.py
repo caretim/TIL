a01ed567ee84e38ef1167ae94f1bf7b9d4a9d8a7
caretim <karesin72041@gmail.com>
2023-03-06 23:37:22 +0900

    2303006

--- WID_T/test.py
+++ WID_T/test.py
@@ -1,4 +1,42 @@
-from math import factorial
+num = [0,1,2,3,4,5,6,7,8,9]
+# 망가진 리모컨 버튼 숫자 빼기
 
+# 1.남은 리모컨 숫자들로 채널에 가까운 숫자 만들기 ( 절대값으로 최소 값 구하기 )
+# 1-1.리스트에서  |절대값|으로 가야할 채널에 가까운 숫자 만들기 -채널번호 for문 돌면서 가까운수, 
+# 100에서 +1,-1로 움직여서 만들 수 있는 횟수 계산하기 -> 두개 넣어서 횟수 최소값 출력
+# except = 채널번호가 100일경우 0 
 
-print(factorial((int(input()))))
+n= int(input())
+
+m= int(input())
+
+k= list(map(int,input().split()))
+
+result = []
+
+for i in k:
+    num.remove(i)
+
+char=''
+
+str_n=str(n)
+
+for s in str_n:
+    i = int(s)
+    add_num =-1
+    check_add = 10
+    for j in num: # 흠 0이면 ? 상관없구나, 
+        if abs(i-j)<check_add:
+            check_add =abs(i-j)
+            add_num =j
+    char+=str(add_num)
+
+result.append(int(char))
+
+result.append(abs(n-100))
+
+r = min(result)
+
+if n == 100:
+    r=0
+print(r)
