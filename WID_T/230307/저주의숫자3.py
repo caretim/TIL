a8a459c4c77335d0ac72768277c903fb8f3c078e
caretim <karesin72041@gmail.com>
2023-03-08 02:33:14 +0900

    230308

---
+++ WID_T/230307/저주의숫자3.py
@@ -0,0 +1,11 @@
+def solution(n):
+    num=[]
+    for i in range(0,200):
+        ic= str(i)
+        if i%3 !=0 and '3' not in ic:
+            num.append(i)     
+    answer = num[n]
+    print(num)
+    return answer
+
+print(solution(int(input())))
