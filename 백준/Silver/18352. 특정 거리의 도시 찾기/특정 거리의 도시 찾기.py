c15dd5001dde8eb5352c3c3bac560bdc6695223f
Timothy Ha <108650777+caretim@users.noreply.github.com>
2023-04-22 23:15:56 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 2012 ms, Memory: 171176 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -0,0 +1,44 @@
+import sys
+import heapq
+
+INF = sys.maxsize
+
+N, M, K, X = map(int, input().split())
+
+
+arr = [INF] * (N + 1)
+
+graph = [[] for __ in range(N + 1)]
+
+
+for __ in range(M):
+    node, wei = map(int, input().split())
+    graph[node].append(wei)
+
+
+def dijkstra(n, k):
+    result = []
+    q = []
+    arr[n] = 0
+    heapq.heappush(q, (0, n))
+    while q:
+        w, node = heapq.heappop(q)
+        for next_node in graph[node]:
+            cost = w + 1
+            if arr[next_node] > cost:
+                arr[next_node] = cost
+                heapq.heappush(q, (cost, next_node))
+    for i in range(1, len(arr)):
+        if arr[i] == k:
+            result.append(i)
+
+    return result
+
+
+r = dijkstra(X, K)
+
+if len(r) == 0:
+    print(-1)
+else:
+    for i in r:
+        print(i)
