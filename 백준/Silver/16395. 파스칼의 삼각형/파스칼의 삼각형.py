55a4e13fd45b12aa4e4b86477063ec5c7703c2cb
Timothy Ha <108650777+caretim@users.noreply.github.com>
2023-06-21 21:52:29 +0900

    [Silver V] Title: 파스칼의 삼각형, Time: 116 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/16395. 파스칼의 삼각형/파스칼의 삼각형.py
@@ -0,0 +1,13 @@
+n,m =map(int,input().split())
+graph =[[0]*(n+1) for __ in range(n+1)]
+
+graph[0][0]=1
+
+for i in range(0,n+1):
+    for j in range(n+1):
+        if j == 0 :
+            graph[i][j]=1
+        else:
+            graph[i][j]= graph[i-1][j-1]+graph[i-1][j]
+
+print(graph[n-1][m-1])
