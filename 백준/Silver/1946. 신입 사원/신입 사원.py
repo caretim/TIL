08e25afe6e36c9acf6be8847251168c6e08e3130
Timothy Ha <108650777+caretim@users.noreply.github.com>
2023-06-26 18:57:24 +0900

    [Silver I] Title: 신입 사원, Time: 5864 ms, Memory: 218332 KB -BaekjoonHub

---
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -0,0 +1,19 @@
+import sys
+
+input =sys.stdin.readline
+
+def solution(scores):
+    result = 0
+    max_rank = [100001, 100001]
+    for score in sorted(scores):
+        if max_rank[1] > score[1]:
+            max_rank = score
+            result += 1
+    return result
+
+
+
+for __ in range(int(input())):
+    n = int(input())
+    scores = [list(map(int, input().split())) for _ in range(n)]
+    print(solution(scores))
