fe577b653d62018a0d589e6891ff2ed312280ef5
Timothy Ha <108650777+caretim@users.noreply.github.com>
2023-06-29 23:48:36 +0900

    [Silver IV] Title: 점화식, Time: 148 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Silver/13699. 점화식/점화식.py
@@ -0,0 +1,21 @@
+n= int(input())
+
+dp = [0 for __ in range(36)]
+
+dp[0]=1
+dp[1]=1
+dp[2]=2
+dp[3]=5
+#t(n-1)*t(0)
+# t(1)=t(0)*t(0)=1
+# t(2)=t(0)*t(1)+t(1)*t(0)=2
+# t(3)=t(0)*t(2)+t(1)*t(1)+t(2)*t(0)=5
+
+if n>3:
+
+    for i in range(4,n+1):
+        for j in range(i):
+            dp[i]+= dp[j]*dp[i-j-1]
+
+print(dp[n])
+     
