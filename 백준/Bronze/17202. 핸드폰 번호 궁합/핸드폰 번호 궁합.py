569ce126dbc68b47f185f88803f302639a5ef17a
Timothy Ha <108650777+caretim@users.noreply.github.com>
2023-07-04 18:23:33 +0900

    [Bronze I] Title: 핸드폰 번호 궁합, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/17202. 핸드폰 번호 궁합/핸드폰 번호 궁합.py
@@ -0,0 +1,25 @@
+
+#10-3619-5974 이면, 7346715995393764에서 시작하여 070386484822030, 77314022204233, 4045424424656, 449966866011, 83852442612, 1137686873, 240344450, 64378895, 0705674, 775131, 42644, 6808, 488, 26
+
+a = input()
+b = input()
+ 
+number = ""
+
+for i in range(len(a)):
+    number += a[i]
+    number += b[i]
+ 
+result = number
+
+
+ 
+while len(result) != 2:
+    num = []
+    for i in range(len(result)-1):
+       new_num = (int(result[i])+int(result[i+1]))%10
+       num.append(str(new_num))
+    result=num
+
+
+print(*result,sep='')
