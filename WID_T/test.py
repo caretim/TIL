aea4b4d504b4000bfde8e1823a9afe80e64b0880
caretim <karesin72041@gmail.com>
2023-08-30 21:34:22 +0900

    LinkedList

--- WID_T/test.py
+++
@@ -1,8 +0,0 @@
-
-for a, b, c in permutations(mid, 3):
-    total = dist[x][a] + dist[a][b] + dist[b][c] + dist[c][z]
-    if result > total:
-        result = total
-if result == INF:
-    result = -1
-print(result)
