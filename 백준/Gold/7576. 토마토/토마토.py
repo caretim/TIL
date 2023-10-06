117f9d485883e22b34f083dc9afe1ac12e63cfbf
Timothy Ha <108650777+caretim@users.noreply.github.com>
2023-10-06 22:24:58 +0900

    [Gold V] Title: 토마토, Time: 2340 ms, Memory: 154464 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.py
@@ -0,0 +1,36 @@
+from collections import deque
+
+n, m = map(int, input().split())
+
+matrix = [list(map(int, input().split())) for __ in range(m)]
+
+dx = [0, 0, 1, -1]
+dy = [-1, 1, 0, 0]
+
+check = [[0] * n for __ in range(m)]
+
+result = 0
+
+start_list = []
+for y in range(m):
+    for x in range(n):
+        if matrix[y][x] == 1:
+            start_list.append((y, x, 0))
+            check[y][x] = 1
+
+q = deque(start_list)
+while q:
+    y, x, c = q.popleft()
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        if 0 <= ny < m and 0 <= nx < n:
+            if matrix[ny][nx] == 0:
+                matrix[ny][nx] = 1
+                q.append((ny, nx, c + 1))
+
+for i in matrix:
+    if i.count(0):
+        c = -1
+
+print(c)
