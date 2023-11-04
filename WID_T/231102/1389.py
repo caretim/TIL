f2f0d7c8d21f3f4b933956f634c6287589474f77
caretim <karesin72041@gmail.com>
2023-11-04 19:31:59 +0900

    231104

--- WID_T/231102/1389.py
+++ WID_T/231102/1389.py
@@ -3,39 +3,43 @@ import sys
 
 input = sys.stdin.readline
 
+n, m = map(int, input().split())
+
+graph = [[] for __ in range(n)]
+
+
+for __ in range(m):
+    u, v = map(int, input().split())
+    u -= 1
+    v -= 1
+    graph[u].append(v)
+    graph[v].append(u)
+
 
 def bfs(start):
     q = deque([])
     q.append(start)
-    visit = [0] * (n + 1)
+    visit = [0] * (n)
     visit[start] = 1
     while q:
-        num = q.popleft()
-        for i in adj[num]:
-            if not visit[i]:
-                visit[i] = visit[num] + 1
-                q.append(i)
-
-    for v in range(1, n + 1):
-        visit[v] -= 1
-
-    return sum(visit)
+        now = q.popleft()
+        for next in graph[now]:
+            if visit[next] == 0:
+                visit[next] = visit[now] + 1
+                q.append(next)
+    print(visit)
+    return sum(visit) - 1
 
 
-n, m = map(int, input().split())
-lst = []
+result = 0
+min = sys.maxsize
 
-for _ in range(m):
-    lst.extend(list(map(int, sys.stdin.readline().split())))
+for i in range(n):
+    cnt = bfs(i)
+    if cnt < min:
+        min = cnt
+        result = i
+    # result.append(bfs(i))
 
-adj = [[] for _ in range(n + 1)]
-for i in range(m):
-    n1, n2 = lst[i * 2], lst[i * 2 + 1]
-    adj[n1].append(n2)
-    adj[n2].append(n1)
 
-answer = []
-for i in range(1, n + 1):
-    answer.append(bfs(i))
-
-print(answer.index(min(answer)) + 1)
+print(result + 1)
