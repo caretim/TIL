f6e755e67a151423acc24a9024a27b74f70b642a
Timothy Ha <108650777+caretim@users.noreply.github.com>
2024-01-20 16:24:18 +0900

    [Silver IV] Title: 돌 게임 2, Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.py
@@ -0,0 +1,22 @@
+n = int(input())
+
+dp = [0] * 10001
+
+
+dp[1] = 1
+dp[2] = 2
+dp[3] = 1
+dp[4] = 2
+
+for i in range(5, n + 1):
+    if dp[i - 1] == 1 or dp[i - 3] == 1:
+        dp[i] = 2
+    else:
+        dp[i] = 1
+
+
+# print(dp)
+if dp[n] != 1:
+    print("SK")
+else:
+    print("CY")
