d41fbc1be2799cf0d77260821638b3b75be277ed
caretim <karesin72041@gmail.com>
2024-04-05 23:44:00 +0900

    240405

--- WID_T/240403/2021.py
+++ WID_T/240403/2021.py
@@ -10,7 +10,8 @@ station = [[] for __ in range(n+1)]
 train = []
 
 
-visited = [INF]*(n+1)
+station_visited = [INF]*(n+1)
+train_visited = [INF]*(m+1)
 
 
 
@@ -20,15 +21,34 @@ for i in range(m):
     for t in range(len(TL)-1):
         station[TL[t]].append(i)
 
-
 start,end = map(int,input().split())
 
+start= set(station[start])
+end = set(station[end])
 
-def dijkstra(start):
-    visited[start] =0
-    heap= []
-    heapq.heappush(heap,((0,start)))
-    
-    while heap:
-        cnt,now =heapq.heappop(heap)
-        if visited[now] 
+
+def dijkstra(start,end):
+    set_ = set()
+    for s in start:
+        train_visited[s] =0
+        set_.add((0,s))
+    while set_:
+        cnt,now =set_.pop()
+        if now in end:
+            return cnt
+
+        if train_visited[now] < cnt:
+            continue
+
+        for line in train[now]:
+            if train_visited[line]<cnt:
+                continue
+
+            for t in station[line]:
+                if train_visited[t]>cnt+1:
+                    train_visited[t]=cnt+1
+                    set_.add((cnt+1,t))
+        
+
+print(dijkstra(start,end))
+
---
+++ WID_T/test.py
@@ -0,0 +1,6 @@
+
+
+b= ['a']
+
+if 'a' in b:
+    print(1)
