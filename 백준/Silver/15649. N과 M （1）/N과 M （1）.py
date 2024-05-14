a12c9e4da15ab00bf18c8522cc9d46220e63b100
Timothy Ha <108650777+caretim@users.noreply.github.com>
2024-05-14 18:07:40 +0900

    [Silver III] Title: N과 M (1), Time: 180 ms, Memory: 112580 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,15 +1,13 @@
-import itertools 
+stack = []
+def backtraking(m):
+    if len(stack)==m:
+        print(' '.join(map(str,stack))) # 현재 함수 종료조건 - 문제의 최대 길이 도달시 
+        return
+    for i in range(1,n+1):  
+        if i not in stack:   #현재 값이 스택에 안들어가 있다면, 값 추가 후 재귀적으로 탐색시행
+            stack.append(i)
+            backtraking(m)   
+            stack.pop()
 
-
-n,m= map(int,input().split())
-
-num_list= []
-
-for num in range(1,n+1):
-    num_list.append(num)
-
-
-result = list(itertools.permutations(num_list,m))
-
-for r in result:
-    print(*r)
+n,m = map(int,input().split())
+backtraking(m)
