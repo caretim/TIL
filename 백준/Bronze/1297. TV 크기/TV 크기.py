b8067bbb5cfd64343e18cf3b36c88caec2eb5a5b
Timothy Ha <108650777+caretim@users.noreply.github.com>
2024-10-26 13:59:21 +0700

    [Bronze II] Title: TV 크기, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/1297. TV 크기/TV 크기.py
+++ 백준/Bronze/1297. TV 크기/TV 크기.py
@@ -1,5 +1,21 @@
-d, h, w = map(int, input().split())
-temp = d / ((h ** 2 + w ** 2) ** 0.5) # 대각
-H = int(h * temp)
-W = int(w * temp)
-print(H, W)
+import sys
+
+input =sys.stdin.readline
+
+
+
+#대각선의 길이 , h,w 높이 너비 비율 
+
+# 피타고라스의 정리 대각선 제곱 값은 너비제곱,높이제곱의 합과 같음
+# 대각선 제곱/h+w 
+
+d,h,w =map(int,input().split())
+
+
+
+sq = (d**2/((h**2)+(w**2)))**(1/2)
+
+
+print(int(h*sq),int(w*sq))
+
+
